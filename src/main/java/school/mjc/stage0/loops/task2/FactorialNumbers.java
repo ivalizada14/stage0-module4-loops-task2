c3375fd7097a1c446db37ca5962a9a0ee8134492
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-01 20:48:23 +0300

    first

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
         int factorial = 1, n = 1;
-        while(printToInclusive<= factorial)
+        while(printToInclusive >= factorial)
         {
             factorial *= n;
             System.out.println(factorial);
